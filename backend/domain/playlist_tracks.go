101bdfa810229b97b49d2dda0e0c1e637a4dfabe
Михаил Якименко <rik.zunqq@gmail.com>
2025-11-05 02:56:18 +0300

    fix

--- backend/domain/playlist_tracks.go
+++ backend/domain/playlist_tracks.go
@@ -1,9 +1,9 @@
 package domain
 
 type PlaylistTracksEntity struct {
-	PlaylistID       uint  `gorm:"column:playlist_id;        primaryKey"`
-	TrackID          uint  `gorm:"column:track_id;           primaryKey"`
-	OriginPlaylistID *uint `gorm:"column:origin_playlist_id; primaryKey"`
+	PlaylistID       uint  `gorm:"column:playlist_id; primaryKey"`
+	TrackID          uint  `gorm:"column:track_id"`
+	OriginPlaylistID *uint `gorm:"column:origin_playlist_id"`
 
 	Playlist       *PlaylistEntity `gorm:"foreignKey:PlaylistID"`
 	Track          *TrackEntity    `gorm:"foreignKey:TrackID"`
