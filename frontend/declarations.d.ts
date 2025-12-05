d0984e74c035f87c0e32f8e19b38cc2106781cbe
Михаил Якименко <rik.zunqq@gmail.com>
2025-12-05 19:06:36 +0300

    update root

--- frontend/declarations.d.ts
+++ frontend/declarations.d.ts
@@ -1,6 +1,10 @@
 declare global {
     const ROOT: HTMLDivElement
 
+    interface Window {
+        readonly ROOT: HTMLDivElement
+    }
+
     declare const ENV: {
         readonly APP_URL: string
     }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
 
-const container = document.getElementById("root")!
-;(window as any).ROOT = container
-
-createRoot(container)
+createRoot(ROOT)
     .render(<App />)
