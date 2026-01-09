60fd8f95cca7f91bd05764fa2ab7e72a4ca4dbd8
Михаил Якименко <rik.zunqq@gmail.com>
2026-01-10 02:32:31 +0300

    add directory handle for dnd

--- frontend/src/components/DragAndDrop/DragAndDrop.tsx
+++ frontend/src/components/DragAndDrop/DragAndDrop.tsx
@@ -31,14 +31,50 @@ export function DragAndDrop(props: DragAndDropProps) {
                     hovered.set(false)
                 }, 10)
             }}
-            onDrop={(e) => {
+            onDrop={async(e) => {
                 e.preventDefault()
                 clearTimeout(timeout.current ?? undefined)
 
-                props.onChange(Array.from(e.dataTransfer.files))
+                const allFiles: File[] = []
+                for (const item of Array.from(e.dataTransfer.items)) {
+                    const entry = item.webkitGetAsEntry()
+
+                    if (entry) {
+                        const files = await traverseFileTree(entry)
+                        allFiles.push(...files)
+                    } else if (item.kind === "file") {
+                        const file = item.getAsFile()
+                        if (file) allFiles.push(file)
+                    }
+                }
+
+                props.onChange(allFiles)
                 hovered.set(false)
             }}
             children={props.children}
         />
     )
+}
+
+async function traverseFileTree(entry: FileSystemEntry): Promise<File[]> {
+    if (entry.isFile) {
+        const fileEntry = entry as FileSystemFileEntry
+        const file: File = await new Promise((resolve) => fileEntry.file(resolve))
+
+        return [file]
+    }
+
+    if (!entry.isDirectory) return []
+
+    const directory = entry as FileSystemDirectoryEntry
+    const reader = directory.createReader()
+    const entries: FileSystemEntry[] = await new Promise((resolve) => reader.readEntries(resolve))
+
+    const allFiles: File[] = []
+    for (const entry of entries) {
+        const files = await traverseFileTree(entry)
+        allFiles.push(...files)
+    }
+
+    return allFiles
 }
