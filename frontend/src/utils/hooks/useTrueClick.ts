aa559601a46ecb74ab167e50383af02ac92f4390
Михаил Якименко <rik.zunqq@gmail.com>
2026-01-10 02:30:21 +0300

    move handleEnter

--- frontend/src/utils/hooks/useTrueClick.ts
+++ frontend/src/utils/hooks/useTrueClick.ts
@@ -1,4 +1,3 @@
-import { handleEnter } from "@utils/events"
 import { ReactEvent } from "@utils/react"
 import { useRef } from "react"
 
@@ -42,4 +41,31 @@ export function useTrueClick<T extends Element>(handler?: TrueClickHandler<T> | 
         onPointerCancel,
         onKeyUp: handleEnter
     }
+}
+
+function handleEnter<T extends Element>(e: ReactEvent.Keyboard<T>) {
+    if (e.key !== "Enter") return
+    e.preventDefault()
+
+    const target = e.target as HTMLElement
+    const rect = target.getBoundingClientRect()
+
+    const init = {
+        bubbles: true,
+        cancelable: true,
+        composed: true,
+        clientX: Math.round(rect.left + rect.width / 2),
+        clientY: Math.round(rect.top + rect.height / 2),
+        pointerId: 1,
+        width: 1,
+        height: 1,
+        pressure: 0.5,
+        isPrimary: true,
+        pointerType: "mouse",
+    }
+
+    target.dispatchEvent(new PointerEvent("pointerdown", init))
+
+    init.pressure = 0
+    target.dispatchEvent(new PointerEvent("pointerup", init))
 }
