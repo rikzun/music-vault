40df10ccd984dd0507c9b662b17a45b1e1670242
Михаил Якименко <rik.zunqq@gmail.com>
2026-01-14 23:49:18 +0300

    optimized resize

--- frontend/src/workers/track.worker.ts
+++ frontend/src/workers/track.worker.ts
@@ -1,15 +1,16 @@
-import { TrackData, TrackImage } from "@components/sidebar/SidebarUpload/SidebarUpload.types"
+import { IUploadTrackMeta, TrackImage } from "@components/sidebar/SidebarUpload"
 import { type IAudioMetadata, parseBlob } from "music-metadata"
 import * as Comlink from "comlink"
 import { WorkerLib } from "@workers/lib"
+import { trimAndNullIfEmpty } from "@utils/std"
 
 export class TrackWorkerRPC extends WorkerLib.RPC {
-    imageSize = 1024
+    imageSize = 512
 
     async checkCanvasSupport() {
         try {
             const canvas = new OffscreenCanvas(this.imageSize, this.imageSize)
-            if (canvas.getContext("2d") == null) throw Error()
+            if (canvas.getContext("bitmaprenderer") == null) throw Error()
 
             return true
         } catch {
@@ -17,62 +18,74 @@ export class TrackWorkerRPC extends WorkerLib.RPC {
         }
     }
 
-    async resizeImage(data: Uint8Array, format: string): Promise<TrackImage> {
-        const blob = new Blob([data as BlobPart], { type: format })
-        const imageBitmap = await createImageBitmap(blob)
+    async resizeImage(data: Uint8Array, format: string): Promise<TrackImage | null> {
+        const startBlob = new Blob([data as BlobPart], { type: format })
 
-        if (imageBitmap.height <= this.imageSize && imageBitmap.width <= this.imageSize) {
-            return {
-                data: await blob.arrayBuffer(),
-                objectURL: URL.createObjectURL(blob)
-            }
-        }
-    
+        const imageBitmap = await createImageBitmap(startBlob, {
+            resizeWidth: this.imageSize,
+            resizeHeight: this.imageSize,
+            resizeQuality: "high"
+        })
+
         const canvas = new OffscreenCanvas(this.imageSize, this.imageSize)
-        const ctx = canvas.getContext("2d")!
-        ctx.imageSmoothingEnabled = true
-        ctx.imageSmoothingQuality = "high"
-    
-        ctx.drawImage(
-            imageBitmap,
-            0, 0,
-            this.imageSize, this.imageSize
-        )
-    
-        const finalImageBlob = await canvas.convertToBlob()
-        const finalImageData = await finalImageBlob.arrayBuffer()
-    
+        const ctx = canvas.getContext("bitmaprenderer")!
+
+        ctx.transferFromImageBitmap(imageBitmap)
+        imageBitmap.close()
+
+        const blob = await canvas.convertToBlob()
+
         return {
-            data: finalImageData,
-            objectURL: URL.createObjectURL(finalImageBlob)
+            blob: blob,
+            objectURL: URL.createObjectURL(blob)
         }
     }
 
-    async parseMeta(files: File[]) {
-        return (await Promise.all(files.map(async(file) => {
-            let meta: IAudioMetadata
+    async parseMeta(file: File) {
+        let meta: IAudioMetadata
     
-            try {
-                meta = await parseBlob(file)
-            } catch {
-                return null
-            }
+        try {
+            meta = await parseBlob(file)
+        } catch {
+            return null
+        }
 
-            console.log(meta)
+        console.log(meta)
     
-            const image = meta.common.picture?.[0]
-            const imageData = image?.data
-            const imageFormat = image?.format   
-    
-            if (
-                image == null ||
-                (imageData == null || imageData.length == 0) ||
-                (imageFormat == null || imageFormat == "")
-            ) return new TrackData(file, meta)
-    
-            const resizedImage = await this.resizeImage(imageData, imageFormat)
-            return new TrackData(file, meta, resizedImage)
-        }))).filter(Boolean) as TrackData[]
+        const image = meta.common.picture?.[0]
+        const imageData = image?.data
+        const imageFormat = image?.format
+
+        const bitrate = meta.format.bitrate
+        
+        const artist = meta.common.artists
+            ?.map(trimAndNullIfEmpty)
+            .filter(Boolean) as string[] | null
+
+        const data: IUploadTrackMeta = {
+            image: null,
+            title: trimAndNullIfEmpty(meta.common.title),
+            artists: artist ?? [],
+            album: trimAndNullIfEmpty(meta.common.album),
+            codec: trimAndNullIfEmpty(meta.format.codec),
+            bitrate: bitrate ? Math.round(bitrate) : null,
+            lossless: meta.format.lossless ?? false
+        }
+
+        if (
+            image == null ||
+            (imageData == null || imageData.length == 0) ||
+            (imageFormat == null || imageFormat == "")
+        ) return data
+
+        const resizedImage = await this.resizeImage(imageData, imageFormat)
+
+        if (resizedImage) {
+            data.image = resizedImage
+            return data
+        }
+
+        return data
     }
 }
 
