92098ac9685188762ef40700a525b9fb7fc9fea6
Михаил Якименко <rik.zunqq@gmail.com>
2026-01-18 03:04:58 +0300

    update uploadtrackprogress

--- frontend/src/components/common/UploadTrackProgress/UploadTrackProgress.tsx
+++ frontend/src/components/common/UploadTrackProgress/UploadTrackProgress.tsx
@@ -4,12 +4,25 @@ import { UploadTrackProgressProps } from "./UploadTrackProgress.types"
 import { ReactNode } from "react"
 import { CircleProgress } from "@components/common/CircleProgress"
 import CheckRounded from "@mui/icons-material/CheckRounded"
+import ErrorOutlineRounded from "@mui/icons-material/ErrorOutlineRounded"
 
 export function UploadTrackProgress(props: UploadTrackProgressProps) {
     const imageObjectURL = props.data.meta?.image?.objectURL ?? null
 
     const title = props.data.meta!.title!
     const artists = props.data.meta!.artists
+
+    let rightElement: ReactNode = null
+
+    if (props.data.errorStatus != null) {
+        rightElement = <ErrorOutlineRounded />
+    } else {
+        if (props.data.progress === 100) {
+            rightElement = <CheckRounded />
+        } else {
+            rightElement = <CircleProgress value={props.data.progress ?? 0} size={28} />
+        }
+    }
 
     return (
         <div className="upload-track-progress-component">
@@ -37,12 +50,9 @@ export function UploadTrackProgress(props: UploadTrackProgressProps) {
                     </div>
                 </div>
 
-                {props.data.progress !== 0 &&
+                {rightElement != null &&
                     <div className="right">
-                        {props.data.progress === 100
-                            ? <CheckRounded />
-                            : <CircleProgress value={props.data.progress} size={28} />
-                        }
+                        {rightElement}
                     </div>
                 }
             </div>
